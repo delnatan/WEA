9f68397c0840d539fbc2610e44fd7d0a05f735aa
Daniel Elnatan <4480843+delnatan@users.noreply.github.com>
2022-07-11 16:17:31 -0700

    trained tubulin-nucleus model from scratch; model v3c

--- WEA/core.py
+++ WEA/core.py
@@ -73,7 +73,7 @@ logger.addHandler(ch)
 
 DEFAULT_CYTO_PATH = str(__model_dir / "CP_bcat-nuc_v3c")
 DEFAULT_NUC_PATH = str(__model_dir / "CP_dapi_v01")
-DEFAULT_TUBASCYTO_PATH = str(__model_dir / "CP_tub-nuc_v3")
+DEFAULT_TUBASCYTO_PATH = str(__model_dir / "CP_tub-nuc_v3c")
 
 logger.info(f"Using models from {str(__model_dir)}")
 

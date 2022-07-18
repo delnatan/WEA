7e738c7cce85d60fc1cd4210015346e38352c4bf
Daniel Elnatan <4480843+delnatan@users.noreply.github.com>
2022-07-18 11:33:14 -0700

    added inj-gfp model for testing

--- WEA/utils.py
+++ WEA/utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+import tifffile
+from pathlib import Path
 from scipy.ndimage import gaussian_filter, maximum_filter, median_filter
 
 
@@ -17,3 +19,24 @@ def det_norm_hess(img, r=1.8):
     detHimg = t * t * (Lxx * Lyy - Lxy * Lxy)
 
     return detHimg
+
+
+def save_as_cellpose_input(layers, outdir, fnout, mode="nuclei"):
+    outpath = Path(outdir)
+    dapi = [layer.data for layer in layers if layer.name.startswith("dapi")][0]
+    tubulin = [layer.data for layer in layers if layer.name.startswith("tubulin")][0]
+    cyto = [layer.data for layer in layers if layer.name.startswith("cyto")][0]
+
+    Ny, Nx = dapi.shape
+
+    rgb = np.zeros((Ny, Nx, 3), dtype=np.uint16)
+
+    if mode == "nuclei":
+        rgb[:, :, 0] = tubulin
+        rgb[:, :, 1] = dapi
+    else:
+        rgb[:, :, 0] = tubulin
+        rgb[:, :, 1] = cyto
+        rgb[:, :, 2] = dapi
+
+    tifffile.imwrite(outpath / fnout, rgb)
